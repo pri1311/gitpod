bb8bd9a9157ec8a856369f41eeec9a3a7d8fbf56
Pudong Zheng <tianshi8650@gmail.com>
2021-12-03 18:06:43 +0000

    [image-builder] fix forcing tags

--- components/image-builder-mk3/pkg/resolve/resolve.go
+++ components/image-builder-mk3/pkg/resolve/resolve.go
@@ -6,6 +6,9 @@ package resolve
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"strings"
 	"sync"
 	"time"
@@ -13,12 +16,14 @@ import (
 	"github.com/containerd/containerd/remotes"
 	dockerremote "github.com/containerd/containerd/remotes/docker"
 	"github.com/docker/distribution/reference"
+	"github.com/opencontainers/go-digest"
 	"github.com/opentracing/opentracing-go"
 	"golang.org/x/xerrors"
 
 	"github.com/gitpod-io/gitpod/common-go/log"
 	"github.com/gitpod-io/gitpod/common-go/tracing"
 	"github.com/gitpod-io/gitpod/image-builder/pkg/auth"
+	ociv1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 // ErrNotFound is returned when the reference was not found
@@ -47,7 +52,7 @@ func (sr *StandaloneRefResolver) Resolve(ctx context.Context, ref string, opts .
 		r = dockerremote.NewResolver(dockerremote.ResolverOptions{
 			Authorizer: dockerremote.NewDockerAuthorizer(dockerremote.WithAuthCreds(func(host string) (username, password string, err error) {
 				if options.Auth == nil {
-					return
+					return "", "", nil
 				}
 
 				return options.Auth.Username, options.Auth.Password, nil
@@ -79,14 +84,55 @@ func (sr *StandaloneRefResolver) Resolve(ctx context.Context, ref string, opts .
 	}
 
 	nref := pref.String()
+	pref = reference.TrimNamed(pref)
 	span.LogKV("normalized-ref", nref)
 
-	res, _, err = r.Resolve(ctx, nref)
+	res, desc, err := r.Resolve(ctx, nref)
 	if err != nil && strings.Contains(err.Error(), "not found") {
 		err = ErrNotFound
 	}
+	fetcher, err := r.Fetcher(ctx, res)
+	if err != nil {
+		return "", err
+	}
 
-	return
+	in, err := fetcher.Fetch(ctx, desc)
+	if err != nil {
+		return "", err
+	}
+	defer in.Close()
+	buf, err := ioutil.ReadAll(in)
+	if err != nil {
+		return "", err
+	}
+
+	var mf ociv1.Manifest
+	err = json.Unmarshal(buf, &mf)
+	if err != nil {
+		return "", fmt.Errorf("cannot unmarshal manifest: %w", err)
+	}
+
+	if mf.Config.Size != 0 {
+		pref, err = reference.WithDigest(pref, mf.Config.Digest)
+		return pref.String(), nil
+	}
+
+	var mfl ociv1.Index
+	err = json.Unmarshal(buf, &mfl)
+	if err != nil {
+		return "", err
+	}
+
+	var dgst digest.Digest
+	for _, mf := range mfl.Manifests {
+		if fmt.Sprintf("%s-%s", mf.Platform.OS, mf.Platform.Architecture) == "linux-amd64" {
+			dgst = mf.Digest
+			pref, err = reference.WithDigest(pref, dgst)
+			return pref.String(), nil
+		}
+	}
+
+	return "", fmt.Errorf("no manifest for platform linux-amd64 found")
 }
 
 type opts struct {
